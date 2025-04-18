d283fb45449be0ca1c4f330750b9b420ecb64bfe
Laptop-user <gopal2773@gmail.com>
2025-04-18 23:11:47 +1000

    Configuration annotation with application properties

--- src/main/java/com/dreamzlancer/springbasicmvn/SpringbasicmvnApplication.java
+++ src/main/java/com/dreamzlancer/springbasicmvn/SpringbasicmvnApplication.java
@@ -1,5 +1,6 @@
 package com.dreamzlancer.springbasicmvn;
 
+import com.dreamzlancer.springbasicmvn.config.PizzaConfig;
 import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,10 +8,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Log
-public class SpringbasicmvnApplication  {
+public class SpringbasicmvnApplication implements CommandLineRunner {
 
+	public SpringbasicmvnApplication(PizzaConfig pizzaConfig) {
+		this.pizzaConfig = pizzaConfig;
+	}
+
+	private PizzaConfig pizzaConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbasicmvnApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args){
+
+
+		log.info(
+				String.format("I want a %s crust pizza, with %s and %s sauce",
+						pizzaConfig.getCrust(),
+						pizzaConfig.getToppings(),
+						pizzaConfig.getSauce())
+		);
+
 	}
 }
